ef819ab17a8c100c66ad519125bfc783239683c1
Marco Della Rocca <marco741147@gmail.com>
2021-11-02 16:36:05 +0100

    Formatting
    Co-authored-by: Alessandro De Stefano <EarendilTiwele@users.noreply.github.com>

--- include/merge_sort.h
+++ include/merge_sort.h
@@ -1,29 +1,31 @@
 /*
  * Course: High Performance Computing 2020/2021
- * 
+ *
  * Lecturer: Francesco Moscato    fmoscato@unisa.it
- * 
- * Group: 
+ *
+ * Group:
  * De Stefano Alessandro   0622701470  a.destefano56@studenti.unisa.it
  * Della Rocca Marco   0622701573  m.dellarocca22@studenti.unisa.it
- * 
- * OMP implementation of mergesort algorithm 
- * Copyright (C) 2021 Alessandro De Stefano (EarendilTiwele) Marco Della Rocca (marco741)
- * 
+ *
+ * OMP implementation of mergesort algorithm
+ * Copyright (C) 2021 Alessandro De Stefano (EarendilTiwele) Marco Della Rocca
+ * (marco741)
+ *
  * This file is part of OMP Mergesort implementation.
- * 
- * OMP Mergesort implementation is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ *
+ * OMP Mergesort implementation is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * OMP Mergesort implementation is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *
+ * OMP Mergesort implementation is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with OMP Mergesort implementation.  If not, see <http: //www.gnu.org/licenses/>.
+ * along with OMP Mergesort implementation.  If not, see <http:
+ * //www.gnu.org/licenses/>.
  */
 
 #ifndef _MNT_C_USERS_MARCO_DOCUMENTS_UNISA_HPC_CODICE_CONTEST_OMP_INCLUDE_MERGE_SORT_H_
@@ -32,4 +34,4 @@
 void merge_sort(int *X, int n, int *tmp, int task_size);
 void merge(int *X, int n, int *Y, int m, int *tmp);
 
-#endif // _MNT_C_USERS_MARCO_DOCUMENTS_UNISA_HPC_CODICE_CONTEST_OMP_INCLUDE_MERGE_SORT_H_
+#endif  // _MNT_C_USERS_MARCO_DOCUMENTS_UNISA_HPC_CODICE_CONTEST_OMP_INCLUDE_MERGE_SORT_H_
